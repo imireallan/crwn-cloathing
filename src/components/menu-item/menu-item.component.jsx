f2b7638924559e8cc52098f68e16d824986855a2
Allan Imire <imireallan@gmail.com>
2021-01-05 19:07:20 +0300

    add routing and enable menu-items for routing

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import { withRouter, useHistory } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-export const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl, match }) => {
+    const history = useHistory();
     return (
-        <div className={`${size} menu-item`}>
+        <div
+            className={`${size} menu-item`}
+            onClick={() => history.push(`${match.path}${linkUrl}`)}
+        >
             <div
                 className="background-image"
                 style={{ backgroundImage: `url(${imageUrl})` }}
@@ -15,3 +19,5 @@ export const MenuItem = ({ title, imageUrl, size }) => {
         </div>
     );
 };
+
+export default withRouter(MenuItem);
