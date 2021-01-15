41569679e4b21588ebbfe304de1d2efbbfe7d8ae
Allan Imire <imireallan@gmail.com>
2021-01-15 10:05:02 +0300

    refactored menuItem tests

--- src/components/menu-item/menu-item.test.js
+++ src/components/menu-item/menu-item.test.js
@@ -1,30 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import MenuItemWithRouterProps, { MenuItem } from './menu-item.component';
+import { render } from '@testing-library/react';
+import { MenuItem } from './menu-item.component';
 
 describe('MenuItem Component', () => {
     const props = {
         title: 'Mens',
         imageUrl: 'http://imageUrl',
-    }
+    };
     test('renders the "shop now" text', () => {
-        render(<MenuItem />);
-        const spanElement = screen.getByText('shop now');
-        expect(spanElement).toBeInTheDocument();
+        const { getByText } = render(<MenuItem />);
+        getByText('shop now');
     });
-    // TODO: Add check on testing component wrapped in HOC
-    test.skip('MenuItem wrapped with HOC receives router props', () => {
-        const element = render(<MenuItemWithRouterProps/>)
 
-    })
     test('MenuItem renders the "title" prop', () => {
-        render(<MenuItem {...props}/>)
-        const divElement = screen.getByText(/Mens/i)
-        expect(divElement).toBeInTheDocument()
+        const { getByText } = render(<MenuItem {...props} />);
+        getByText('Mens');
     });
 
     test('MenuItem renders a large image if "size" prop is defined', () => {
-        const {container} = render(<MenuItem {...props} size="large"/>)
-        expect(container.firstChild.classList.contains('large')).toBe(true)
-    })
-
-})
+        const { container } = render(<MenuItem {...props} size="large" />);
+        expect(container.firstChild.classList.contains('large')).toBe(true);
+    });
+});
